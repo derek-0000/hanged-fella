415bf76478fa67d6261efe5b0e5421d98780ed6a
Derek Alvarado <derekalvarado24@outlook.com>
2026-01-09 15:42:26 -0600

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,11 @@ import { Elysia } from "elysia";
 import { SlackResponse } from "./core/slack-responses";
 import { slackVerificationMiddleware } from "./core/middleware/slack-verification";
 import { appSchema } from "./core/schemas";
-import staticPlugin from "@elysiajs/static";
 import guessController from "./core/handlers/guess";
 import shareController from "./core/handlers/share";
 import createController from "./core/handlers/create";
-import path from "path";
 
 export default new Elysia()
-  .use(staticPlugin({ assets: path.join(import.meta.dir, "../public") }))
-  .get("/", () => Bun.file(path.join(import.meta.dir, "../public/index.html")))
   .use(slackVerificationMiddleware)
   .post("/slack/events/guess", guessController, appSchema)
   .post("/slack/events/create", createController, appSchema)
