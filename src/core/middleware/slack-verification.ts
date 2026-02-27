5de276a5aa2ee622735693283c398d5a6dd238a5
Derek Alvarado <derekalvarado24@outlook.com>
2026-02-27 16:00:33 -0600

    Work

--- src/core/middleware/slack-verification.ts
+++ src/core/middleware/slack-verification.ts
@@ -11,44 +11,50 @@ type SlackVerificationError = { error: string };
 export async function verifySlackRequest(
   request: Request
 ): Promise<SlackVerificationError | null> {
-  const timestamp = request.headers.get("X-Slack-Request-Timestamp");
-  const slackSignature = request.headers.get("X-Slack-Signature");
-
-  if (!timestamp || !slackSignature) {
-    return { error: "Missing Slack signature headers" };
-  }
-
-  const parsedTimestamp = Number(timestamp);
-  if (!Number.isInteger(parsedTimestamp)) {
-    return { error: "Invalid request timestamp" };
-  }
-
-  const currentTime = Math.floor(Date.now() / 1000);
-  if (Math.abs(currentTime - parsedTimestamp) > 60 * 5) {
-    return { error: "Request timestamp is too old" };
-  }
-
-  const rawBody = await request.clone().text();
-  const sigBasestring = `v0:${timestamp}:${rawBody}`;
-
-  const mySignature =
-    "v0=" +
-    crypto
-      .createHmac("sha256", SLACK_SIGNING_SECRET as string)
-      .update(sigBasestring)
-      .digest("hex");
-
   try {
-    const isValid = crypto.timingSafeEqual(
-      Buffer.from(mySignature),
-      Buffer.from(slackSignature)
-    );
 
-    if (!isValid) {
+    const timestamp = request.headers.get("X-Slack-Request-Timestamp");
+    const slackSignature = request.headers.get("X-Slack-Signature");
+
+    if (!timestamp || !slackSignature) {
+      return { error: "Missing Slack signature headers" };
+    }
+
+    const parsedTimestamp = Number(timestamp);
+    if (!Number.isInteger(parsedTimestamp)) {
+      return { error: "Invalid request timestamp" };
+    }
+
+    const currentTime = Math.floor(Date.now() / 1000);
+    if (Math.abs(currentTime - parsedTimestamp) > 60 * 5) {
+      return { error: "Request timestamp is too old" };
+    }
+
+    const rawBody = await request.clone().text();
+    const sigBasestring = `v0:${timestamp}:${rawBody}`;
+
+    const mySignature =
+      "v0=" +
+      crypto
+        .createHmac("sha256", SLACK_SIGNING_SECRET as string)
+        .update(sigBasestring)
+        .digest("hex");
+
+    try {
+      const isValid = crypto.timingSafeEqual(
+        Buffer.from(mySignature),
+        Buffer.from(slackSignature)
+      );
+
+      if (!isValid) {
+        return { error: "Invalid Slack signature" };
+      }
+    } catch {
       return { error: "Invalid Slack signature" };
     }
-  } catch {
-    return { error: "Invalid Slack signature" };
+  } catch (e) {
+    console.error(e);
+    return { error: "Internal server error" };
   }
 
   return null;
